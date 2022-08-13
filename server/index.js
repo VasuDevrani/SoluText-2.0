619f6c83e73bf8bb52d6dcc3590996245a603ad4
VasuDevrani <vasu2002devrani@gmail.com>
2022-08-13 22:14:53 +0530

    Final push

--- server/index.js
+++ server/index.js
@@ -33,6 +33,7 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'))
